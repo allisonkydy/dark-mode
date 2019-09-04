6b8db2dd37d364951c1be22e2d61d240de62143b
allisonkydy <allisnelly@gmail.com>
2019-09-04 17:12:03 -0500

    add price and price change info, styles

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -11,7 +11,35 @@ const Charts = ({ coinData, darkMode }) => {
           <div className="coin__logo">
             <img src={coin.image} height="40" alt={coin.name} />
           </div>
-          <Chart sparklineData={coin.sparkline_in_7d.price} darkMode={darkMode} />
+          <div className="coin-info">
+            <p>current price: <span>${coin.current_price}</span></p>
+            <p>
+              24 hours:{" "}
+              <span
+                style={
+                  coin.price_change_24h > 0
+                    ? { color: "green" }
+                    : { color: "red" }
+                }
+              >
+                ${coin.price_change_24h}
+              </span>
+              {", "}
+              <span
+                style={
+                  coin.price_change_percentage_24h > 0
+                    ? { color: "green" }
+                    : { color: "red" }
+                }
+              >
+                {coin.price_change_percentage_24h}%
+              </span>
+            </p>
+          </div>
+          <Chart
+            sparklineData={coin.sparkline_in_7d.price}
+            darkMode={darkMode}
+          />
         </div>
       ))}
     </div>
