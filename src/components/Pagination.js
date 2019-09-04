dca282151a1e1fbfa83ddb8ff6a23f65d6df3fee
allisonkydy <allisnelly@gmail.com>
2019-09-04 16:34:22 -0500

    styled pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Pagination = ({ page, changePage }) => {
   return (
-    <div>
-      <p>page {page}</p>
+    <div className="pagination-container">
+      <p>|| page {page} ||</p>
       {page > 1 ? <button onClick={() => changePage('prev')}>prev page</button> : ''}
       <button onClick={() => changePage('next')}>next page</button>
     </div>
