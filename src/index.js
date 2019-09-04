9f1963166a9368176916c50be02501bac7c0adce
allisonkydy <allisnelly@gmail.com>
2019-09-04 16:24:55 -0500

    built out pagination functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import Pagination from "./components/Pagination";
 import { useDarkMode } from './hooks/useDarkMode';
 
 import "./styles.scss";
@@ -11,18 +12,26 @@ import "./styles.scss";
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useDarkMode(false);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=${page}&sparkline=true`
       )
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
-  }, []);
+  }, [page]);
+
+  const changePage = direction => {
+    if (direction === 'next') setPage(page + 1)
+    else if (direction === 'prev') setPage(page - 1)
+  }
+
   return (
     <div className="App">
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Pagination page={page} changePage={changePage} />
       <Charts coinData={coinData} darkMode={darkMode} />
     </div>
   );
